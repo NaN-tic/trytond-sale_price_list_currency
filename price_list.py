9a44253148044126d3700d8b251404bc47577593
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2024-06-05 19:04:21 +0200

    Compute price list in cas has currency or currency rate (default)  (#1)
    #166719

--- price_list.py
+++ price_list.py
@@ -1,20 +1,13 @@
 # This file is part sale_price_list_currency module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 from trytond.model import fields
-from trytond.transaction import Transaction
 
 
 class PriceList(metaclass=PoolMeta):
     __name__ = 'product.price_list'
-    currency = fields.Many2One('currency.currency', 'Currency')
-
-    @classmethod
-    def default_currency(cls):
-        Company = Pool().get('company.company')
-
-        company_id = Transaction().context.get('company')
-        if company_id:
-            company = Company(company_id)
-            return company.currency.id
+    currency = fields.Many2One('currency.currency', 'Currency',
+        help="If a currency different from the company's is specified, "
+            "the price will be based on the price list, and the exchange rate "
+            "of the currency will not be applied.")
